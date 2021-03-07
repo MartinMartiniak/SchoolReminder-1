d318371eb24690664ca89f7ca6131dc8243f245a
misko183 <rtmike183@gmail.com>
2021-03-07 19:55:12 +0100

    Create basic flayout application. Make exit box function.

---
+++ SchoolReminder/SchoolReminder/AppShell.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SchoolReminder.ViewModels;
+using SchoolReminder.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SchoolReminder
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Alert!", "Určite chceš ukončiť aplikáciu?", "Áno", "Nie");
+                if (result) await this.Navigation.PopAsync();
+            });
+
+            return true;
+        }
+    }
+}
---
+++ SchoolReminder/SchoolReminder/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SchoolReminder.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SchoolReminder.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
